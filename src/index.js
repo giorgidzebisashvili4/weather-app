56f974994fbee9189571715be6a85c58e5db2f11
giorgi dzebisashvili <136992517+giorgidzebisashvili4@users.noreply.github.com>
2024-01-31 17:04:28 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+// import Icon from './icon.png';
+
+const containerSelect = document.querySelector(".container");
+
+const test = document.createElement("h1");
+
+test.textContent = "testing css!!";
+
+containerSelect.appendChild(test);
